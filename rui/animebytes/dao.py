1707d08c59bf2c076c3f4285b523ed83fcf05c1f
twissell- <dmaggioesne@gmail.com>
2023-10-25 10:01:01 +0200

    Added request interval for animebytes

--- rui/animebytes/dao.py
+++ rui/animebytes/dao.py
@@ -1,11 +1,12 @@
 import json
 import logging
+from datetime import datetime, timedelta
 from time import sleep
 
 import requests
 
 from rui.animebytes.model import TorrentCollection
-from rui.common import config
+from rui.common import config, persistence
 from rui.common.utils import sanitize
 
 logger = logging.getLogger(__name__)
@@ -18,23 +19,18 @@ def getTorrentCollectionByAnime(anime):
     if anime.title and not rtn:
         rtn = getTorrentCollectionByTitle(anime.title)
     if anime.romaji and not rtn:
-        sleep(1)
         logger.info("Retrying with romaji title.")
         rtn = getTorrentCollectionByTitle(anime.romaji)
     if anime.native and not rtn:
-        sleep(1)
         logger.info("Retrying with native title.")
         rtn = getTorrentCollectionByTitle(anime.native)
     if anime.english and not rtn:
-        sleep(1)
         logger.info("Retrying with english title.")
         rtn = getTorrentCollectionByTitle(anime.english)
     if anime.romaji and not rtn:
-        sleep(1)
         logger.info("Retrying with sanitize romaji title.")
         rtn = getTorrentCollectionByTitle(sanitize(anime.romaji))
     if anime.english and not rtn:
-        sleep(1)
         logger.info("Retrying with sanitize english title.")
         rtn = getTorrentCollectionByTitle(sanitize(anime.english))
 
@@ -42,7 +38,24 @@ def getTorrentCollectionByAnime(anime):
 
 
 def getTorrentCollectionByTitle(title):
+    request_interval = config.get("animebytes.requestInterval") or 4
+
+    last_request = persistence.get("animebytes").get("last_request")
+    if last_request:
+        last_request = datetime.strptime(
+            persistence.get("animebytes")["last_request"], "%Y-%m-%d %H:%M:%S"
+        )
+
+    while last_request and (datetime.now() - last_request) <= timedelta(
+        seconds=request_interval
+    ):
+        logger.debug("Waiting for request interval.")
+        sleep(1)
+
     logger.info('Getting torrents list for "%s"' % title)
+    persistence.set(
+        "animebytes", {"last_request": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+    )
 
     try:
         response = requests.get(
