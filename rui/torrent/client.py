bfc127837b625044f9f4a8c54ff56ceacc8034c1
twissell- <dmaggioesne@gmail.com>
2024-06-17 01:20:47 +0200

    Replaced qbittorent library

--- rui/torrent/client.py
+++ rui/torrent/client.py
@@ -1,4 +1,4 @@
-import qbittorrent
+import qbittorrentapi
 import transmissionrpc
 
 from rui.common import config
@@ -19,19 +19,31 @@ class TransmissionTorrentClient(object):
 class QBitTorrentClient(object):
     def __init__(self, clientConfig=config.get("torrentLoader.torrentClient")):
         super(QBitTorrentClient, self).__init__()
-        self._client = qbittorrent.Client(
-            "http://%s:%s" % (clientConfig["address"], clientConfig["port"])
-        )
-        self._client.login(clientConfig["user"], clientConfig["password"])
 
-    def add(self, torrentFile, destinationPath, name="", category="", tags=""):
-        self._client.set_preferences(
-            start_paused_enabled=config.get("torrentLoader.startPaused"),
-            content_layout="NoSubfolder",
+        conn_info = dict(
+            host=clientConfig["address"],
+            port=clientConfig["port"],
+            username=clientConfig["user"],
+            password=clientConfig["password"],
         )
-        self._client.download_from_link(
-            torrentFile,
-            savepath=destinationPath,
+
+        self._client = qbittorrentapi.Client(**conn_info)
+        try:
+            self._client.auth_log_in()
+        except qbittorrentapi.LoginFailed as e:
+            print(e)
+
+    def add(self, torrentFileUrl, destinationPath, name="", category="", tags=""):
+        self._client.app_setPreferences(
+            prefs={
+                "start_paused_enabled": config.get("torrentLoader.startPaused"),
+                "create_subfolder_enabled": False,
+            }
+        )
+
+        self._client.torrents_add(
+            urls=torrentFileUrl,
+            save_path=destinationPath,
             rename=name,
             category=category,
             tags=tags,
